e1a2a597daaacf638e787b8d9557055c5d7d3306
Samantha Leahy <skleahy1@buffs.wtamu.edu>
2024-09-14 02:50:00 -0500

    email form

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic import ListView
+from .forms import EmailPostForm
 
 # Create your views here.
 def post_list(request):
@@ -35,4 +37,38 @@ def post_detail(request, year, month, day, post):
         request,
         'blog/post/detail.html',
         {'post': post}
+    )
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form
+        }
     )
