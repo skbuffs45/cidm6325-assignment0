7c6310183f391c518d6d810f791d91033e52d019
Samantha Leahy <skleahy1@buffs.wtamu.edu>
2024-10-10 03:59:54 -0500

    adding account app

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 SITE_ID = 1
 
 INSTALLED_APPS = [
+    "account.apps.AccountConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
