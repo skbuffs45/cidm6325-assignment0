d7cb8db10493fe5ff5f7d79bdc4eeed4d2164276
Samantha Leahy <skleahy1@buffs.wtamu.edu>
2024-11-09 03:19:25 -0600

    chapter 8 and 9

---
+++ myshop/orders/views.py
@@ -0,0 +1,42 @@
+from cart.cart import Cart
+from django.shortcuts import get_object_or_404, redirect, render
+from .forms import OrderCreateForm
+from .models import OrderItem
+from .tasks import order_created
+from django.contrib.admin.views.decorators import staff_member_required
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+            # clear the cart
+            cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect('payment:process')
+    else:
+        form = OrderCreateForm()
+    return render(
+        request,
+        'orders/order/create.html',
+        {'cart': cart, 'form': form}
+    )
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(
+        request, 'admin/orders/order/detail.html', {'order': order}
+    )
