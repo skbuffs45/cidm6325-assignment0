76dc7a263dc3292bf53f5264db7be84aaca21503
Samantha Leahy <skleahy1@buffs.wtamu.edu>
2024-11-14 02:31:48 -0600

    starting pdf

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -6,6 +6,10 @@ import datetime
 from django.http import HttpResponse
 from django.urls import reverse
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -44,10 +48,6 @@ def export_to_csv(modeladmin, request, queryset):
         writer.writerow(data_row)
     return response
 export_to_csv.short_description = 'Export to CSV'
-
-def order_detail(obj):
-    url = reverse('orders:admin_order_detail', args=[obj.id])
-    return mark_safe(f'<a href="{url}">View</a>')
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
